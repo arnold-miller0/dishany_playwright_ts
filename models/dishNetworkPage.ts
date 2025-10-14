8f44504115ca095bafaf7f31b09151ca5bb5af53
arnold-miller0 <arnold.miller0@gmail.com>
2025-10-14 16:04:21 -0600

    check display network counts

--- models/dishNetworkPage.ts
+++ models/dishNetworkPage.ts
@@ -5,19 +5,24 @@ import { DishAnywhereBasePage } from './dishBasePage';
 export class DishAnywhereNetworkPage extends DishAnywhereBasePage {
 
 
-    readonly topTopmenu:Locator
+    readonly topTopMenu:Locator
     readonly topNetwork:Locator
+    readonly networksTitle:Locator
 
-    private _topNetText = 'NETWORKS'
-    private _topNetColor = 'rgb(228, 25, 50)' // 
+    private _topNetText:string = 'NETWORKS'
+    private _topNetColor:string  = 'rgb(228, 25, 50)' // 
+    private _networkText:string  = 'Networks'
+    private _netTitleCount:number = -1
+    private _netTitleText:string = ""
    
 
     constructor(page: Page, baseURL: string, webEnv: string) {
         super(page, baseURL, webEnv);
 
          // #top-menu > div.cl.cq.cr.cs.ct.cu.cv.cw.cx.h.i.j.k.l.n.u.w.x > span
-        this.topTopmenu = page.locator('div#top-menu')
-        this.topNetwork = this.topTopmenu.locator('div > span');
+        this.topTopMenu = page.locator('div#top-menu')
+        this.topNetwork = this.topTopMenu.locator('div > span');
+        this.networksTitle = page.locator('span#networks-title')
 
     }
 
@@ -31,6 +36,21 @@ export class DishAnywhereNetworkPage extends DishAnywhereBasePage {
         const rgbColor = await this.getLocColor(this.topNetwork)
         if (debug) console.log('Top Network Text RGB color:', rgbColor);
         expect(rgbColor).toBe(this._topNetColor);
+
+        const numAndTitle = await this.networksTitle.innerText()
+        this._netTitleText = numAndTitle.split(" ")[1]
+        this._netTitleCount = Number(numAndTitle.split(" ")[0])
+        if (debug) console.log(this._networkText, "listed:", this._netTitleCount);
+        expect(this._netTitleText).toBe(this._networkText);
     }
+
+    async getNetTitleText():Promise<string> {
+        return this._netTitleText
+    }
+
+    async getNetTitleCount():Promise<number> {
+        return this._netTitleCount
+    }
+
 
 }
